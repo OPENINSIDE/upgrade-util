d911ed9602077f4114ca39df8b83cd39faf6cf03
Zuhair Hammadi <zuhair.hammadi@open-inside.com>
2025-08-07 14:59:43 +0300

    [FIX] jinja_to_qweb: commit changes for named cursor in verify_upgraded_jinja_fields

--- src/util/jinja_to_qweb.py
+++ src/util/jinja_to_qweb.py
@@ -476,10 +476,11 @@ def verify_upgraded_jinja_fields(cr):
             template_name,
             template_name_field,
             template_converted,
-        ) in ncr:
+        ) in ncr.fetchall():
             if model_name not in env:
                 # custom model not loaded yet. Ignore
                 continue
+            cr.commit()  # commit changes for the named cursor below
             model = env[model_name]
             record = model.with_context({"active_test": False}).search([], limit=1, order="id")
 
